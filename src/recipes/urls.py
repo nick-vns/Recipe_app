86f77e11bc789840b437223bca3c0cc8a232f942
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-08-09 12:52:48 -0600

    added login logout + protection if not loggedin

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -3,7 +3,6 @@ from .views import recipes_home
 from .views import RecipeListView
 from .views import RecipeDetailView
 
-
 app_name = 'recipes'
 
 urlpatterns = [
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/recipes_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
 
 
 def recipes_home(request):
     return render(request, 'recipes/recipes_home.html')
+
+
+@login_required
+def records(request):
+    return render(request, 'recipes/recipes_list.html')
