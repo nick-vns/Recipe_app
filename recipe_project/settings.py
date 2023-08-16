491c4ca1296ca77a7162fdf29e068fc17e780028
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-08-16 12:49:40 -0600

    debugging

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'https://peaceful-castle-07076-327776110990.herokuapp.com', '127.0.0.1']
